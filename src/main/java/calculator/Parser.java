5a68b6c1ef27fc02b40588e9e358848530fb29d0
Jaehyung You (Eric) <bapsajura@naver.com>
2020-02-13 14:09:51 +0900

    Update Parser.java

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Parser {
 
+    private final String BLANK_PATTERN = "\\s* \\s*";
+    private final String BLANK = " ";
     public boolean isNotEmptyOrNull(String s) {
         //try to invoke it on a null reference, I'll get a NullPointerException.
-        if(s == null || s.equals(" ")){
+        if(s == null || s.equals(BLANK)){
             return false;
         }
         return true;
@@ -16,6 +18,7 @@ public class Parser {
     public List<String> splitByBlank(String text) {
         //List.of() needs 1.9+
         //Arrays.asList returns a mutable list while the list returned by List.of is immutable
-        return Arrays.asList(text.split("\\s* \\s*"));
+
+        return Arrays.asList(text.split(BLANK_PATTERN));
     }
 }
