31ecd7ec77b215973aeef741271ad2144534ed2b
Jaehyung You (Eric) <bapsajura@naver.com>
2020-02-13 14:33:23 +0900

    Update Parser.java
    Since Parser instance is making every call

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class Parser {
 
-    private final String BLANK_PATTERN = "\\s* \\s*";
-    private final String BLANK = " ";
+    private static final String BLANK_PATTERN = "\\s* \\s*";
+    private static final String BLANK = " ";
     public boolean isNotEmpty(String s) {
         //try to invoke it on a null reference, I'll get a NullPointerException.
         if(s == null || s.equals(BLANK)){
